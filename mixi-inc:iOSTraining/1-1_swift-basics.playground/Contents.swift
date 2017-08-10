5689a3a72bf7f8e71e422d8aa41c3474aa817315
Kakeru Nakabachi <b1015146@fun.ac.jp>
2017-08-10 10:53:54 +0900

    配列

--- mixi-inc:iOSTraining/1-1_swift-basics.playground/Contents.swift
+++ mixi-inc:iOSTraining/1-1_swift-basics.playground/Contents.swift
@@ -2,4 +2,15 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+/*配列*/
+//可変な配列
+var array1: [String] = [String]()
+array1.append("iPhone6")
+array1 += ["iPhone6s"]
+
+//不変な配列
+let array2: [String] = [String]()
+//array2.append(iPhone7)←できない
+let array3: [String] = ["iPhoneSE", "iPhone6plus"]
+
+
