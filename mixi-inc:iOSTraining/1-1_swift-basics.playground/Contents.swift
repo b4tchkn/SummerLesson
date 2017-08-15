9619dfa0103cf8c0ed0a78a0c0ab4687783f75cf
Kakeru Nakabachi <b1015146@fun.ac.jp>
2017-08-15 17:51:56 +0900

    辞書

--- mixi-inc:iOSTraining/1-1_swift-basics.playground/Contents.swift
+++ mixi-inc:iOSTraining/1-1_swift-basics.playground/Contents.swift
@@ -13,4 +13,16 @@ let array2: [String] = [String]()
 //array2.append(iPhone7)←できない
 let array3: [String] = ["iPhoneSE", "iPhone6plus"]
 
+/*辞書*/
+//可変な辞書
+var dict1: [String : String] = [String : String]()
+dict1["iPhone6s"] = "docomo"
+dict1["iPhone7"] = "au"
+print(dict1)
+
+//不変な辞書
+let dict2: [String : String] = [String : String]()
+//dict2["iPhone6plus"] = ["softbank"]←できない
+let dict3: [String : String] =  ["iPhone6s" : "docomo", "iPhone7" : "au", "iPhone6plus" : "softbank"]
+
 
