093ef544693e19b9d8556455362f03fa7a7895b0
Kakeru Nakabachi <b1015146@fun.ac.jp>
2017-08-15 22:02:34 +0900

    add C#testfile

---
+++ test/test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace test
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int a = 10;
+            Console.WriteLine(a);
+            double b = 1.0;
+            Console.WriteLine(b);
+
+            var c = 10;
+            var d = 5.9;
+
+
+        }
+    }
+}
